41181b2d79e469312257966a8305d99415366dcd
Robert.Rowland <robert.rowland@sega.co.uk>
2018-12-20 16:02:52 +0000

    Added crazy beast!

--- README.md
+++ README.md
@@ -6,7 +6,18 @@ Examples and exercise following Clojure for the Brave and True
 
 In REPL:
 
+```
 (use 'clojure-noob.core :reload)
 (-main)
+attack hobbit!
+{:name right-ear, :size 1}
+attack hobbit!
+{:name chest, :size 10}
+attack beast!
+{:name second-ear, :size 1}
+attack crazy beast!
+{:name thigh-92, :size 4}
+=> "Yahhh!"
+```
 
 Hit some hobbits and beasties and other fun!
--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -2,44 +2,72 @@
   (:gen-class))
 
 (def hobbit-body-parts [{:name "head" :size 3}
-                             {:name "left-eye" :size 1}
-                             {:name "left-ear" :size 1}
+                        {:name "left-eye" :size 1}
+                        {:name "left-ear" :size 1}
+                        {:name "mouth" :size 1}
+                        {:name "nose" :size 1}
+                        {:name "neck" :size 2}
+                        {:name "left-shoulder" :size 3}
+                        {:name "left-upper-arm" :size 3}
+                        {:name "chest" :size 10}
+                        {:name "back" :size 10}
+                        {:name "left-forearm" :size 3}
+                        {:name "abdomen" :size 6}
+                        {:name "left-kidney" :size 1}
+                        {:name "left-hand" :size 2}
+                        {:name "left-knee" :size 2}
+                        {:name "left-thigh" :size 4}
+                        {:name "left-lower-leg" :size 3}
+                        {:name "left-achilles" :size 1}
+                        {:name "left-foot" :size 2}])
+
+(def beast-body-parts [{:name "head" :size 3}
+                       {:name "first-eye" :size 1}
+                       {:name "first-ear" :size 1}
+                       {:name "mouth" :size 1}
+                       {:name "nose" :size 1}
+                       {:name "neck" :size 2}
+                       {:name "first-shoulder" :size 3}
+                       {:name "first-upper-arm" :size 3}
+                       {:name "chest" :size 10}
+                       {:name "back" :size 10}
+                       {:name "first-forearm" :size 3}
+                       {:name "abdomen" :size 6}
+                       {:name "first-kidney" :size 1}
+                       {:name "first-hand" :size 2}
+                       {:name "first-knee" :size 2}
+                       {:name "first-thigh" :size 4}
+                       {:name "first-lower-leg" :size 3}
+                       {:name "first-achilles" :size 1}
+                       {:name "first-foot" :size 2}])
+
+(def crazy-beast-body-parts [{:name "head" :size 3}
+                             {:name "eye-1" :size 1}
+                             {:name "ear-1" :size 1}
                              {:name "mouth" :size 1}
                              {:name "nose" :size 1}
                              {:name "neck" :size 2}
-                             {:name "left-shoulder" :size 3}
-                             {:name "left-upper-arm" :size 3}
+                             {:name "shoulder-1" :size 3}
+                             {:name "upper-arm-1" :size 3}
                              {:name "chest" :size 10}
                              {:name "back" :size 10}
-                             {:name "left-forearm" :size 3}
+                             {:name "forearm-1" :size 3}
                              {:name "abdomen" :size 6}
-                             {:name "left-kidney" :size 1}
-                             {:name "left-hand" :size 2}
-                             {:name "left-knee" :size 2}
-                             {:name "left-thigh" :size 4}
-                             {:name "left-lower-leg" :size 3}
-                             {:name "left-achilles" :size 1}
-                             {:name "left-foot" :size 2}])
+                             {:name "kidney-1" :size 1}
+                             {:name "hand-1" :size 2}
+                             {:name "knee-1" :size 2}
+                             {:name "thigh-1" :size 4}
+                             {:name "lower-leg-1" :size 3}
+                             {:name "achilles-1" :size 1}
+                             {:name "foot-1" :size 2}])
 
-(def beast-body-parts [{:name "head" :size 3}
-                           {:name "first-eye" :size 1}
-                           {:name "first-ear" :size 1}
-                           {:name "mouth" :size 1}
-                           {:name "nose" :size 1}
-                           {:name "neck" :size 2}
-                           {:name "first-shoulder" :size 3}
-                           {:name "first-upper-arm" :size 3}
-                           {:name "chest" :size 10}
-                           {:name "back" :size 10}
-                           {:name "first-forearm" :size 3}
-                           {:name "abdomen" :size 6}
-                           {:name "first-kidney" :size 1}
-                           {:name "first-hand" :size 2}
-                           {:name "first-knee" :size 2}
-                           {:name "first-thigh" :size 4}
-                           {:name "first-lower-leg" :size 3}
-                           {:name "first-achilles" :size 1}
-                           {:name "first-foot" :size 2}])
+(defn matching-variable-ped-part
+  [part]
+  (loop [iteration 2, parts-list #{{:name (:name part) :size (:size part)}}]
+    (if (> iteration 100)
+      parts-list
+      (recur (inc iteration) (conj parts-list {:name (clojure.string/replace (:name part) #"-1$" (str "-" iteration)) :size (:size part)}))))
+  )
 
 (defn matching-quinta-ped-part
   [part]
@@ -87,6 +115,17 @@
         part
         (recur remaining (+ accumulated-size (:size (first remaining))))))))
 
+(defn hit-crazy-a-ped
+  [body-parts]
+  (let [sym-parts (symmetrize-body-parts matching-variable-ped-part body-parts)
+        body-part-size-sum (reduce + (map :size sym-parts))
+        target (rand body-part-size-sum)]
+    (loop [[part & remaining] sym-parts
+           accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur remaining (+ accumulated-size (:size (first remaining))))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
@@ -96,4 +135,6 @@
   (println (hit-byped hobbit-body-parts))
   (println "attack beast!")
   (println (hit-quint-a-ped beast-body-parts))
+  (println "attack crazy beast!")
+  (println (hit-crazy-a-ped crazy-beast-body-parts))
   "Yahhh!")
